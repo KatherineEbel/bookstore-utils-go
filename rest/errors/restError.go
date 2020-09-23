a2c32337570c3de19893329913c3d56fde17d3b6
Katherine Ebel <kathyebel@me.com>
2020-09-23 09:48:32 -0400

    rest errors

---
+++ rest/errors/restError.go
@@ -0,0 +1,46 @@
+package errors
+
+import (
+	"net/http"
+)
+
+type RestError struct {
+	Message string        `json:"message"`
+	Code    int           `json:"code"`
+	Causes  []interface{} `json:"causes"`
+}
+
+func (e *RestError) Error() string {
+	return e.Message
+}
+
+func NewDatabaseError() *RestError {
+	return NewInternalServerError("database error", nil)
+}
+func NewBadRequestError(msg string) *RestError {
+	return &RestError{
+		Message: msg,
+		Code:    http.StatusBadRequest,
+		Causes:  nil,
+	}
+}
+
+func NewNotFoundError(msg string) *RestError {
+	return &RestError{
+		Message: msg,
+		Code:    http.StatusNotFound,
+		Causes:  nil,
+	}
+}
+
+func NewInternalServerError(msg string, errs ...error) *RestError {
+	var causes []interface{}
+	for _, err := range errs {
+		causes = append(causes, err)
+	}
+	return &RestError{
+		Message: msg,
+		Code:    http.StatusInternalServerError,
+		Causes:  causes,
+	}
+}
---
+++ rest/errors/restError_test.go
@@ -0,0 +1,43 @@
+package errors
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewInternalServerError(t *testing.T) {
+	err := NewInternalServerError("my error message", errors.New("my error"))
+	assert.NotNil(t, err)
+	assert.EqualValues(t, err.Code, 500)
+	assert.EqualValues(t, err.Message, "my error message")
+	assert.EqualValues(t, err.Message, err.Error())
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes))
+	assert.EqualValues(t, "my error", err.Causes[0].(error).Error())
+}
+
+func TestNewBadRequestError(t *testing.T) {
+	err := NewBadRequestError("my error message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, err.Code, 400)
+	assert.EqualValues(t, err.Message, "my error message")
+	assert.EqualValues(t, err.Message, err.Error())
+}
+
+func TestNewNotFoundError(t *testing.T) {
+	err := NewNotFoundError("my error message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, err.Code, 404)
+	assert.EqualValues(t, err.Message, "my error message")
+	assert.EqualValues(t, err.Message, err.Error())
+}
+
+func TestNewDatabaseError(t *testing.T) {
+	err := NewDatabaseError()
+	assert.NotNil(t, err)
+	assert.EqualValues(t, err.Code, 500)
+	assert.EqualValues(t, err.Message, "database error")
+	assert.EqualValues(t, err.Message, err.Error())
+}
